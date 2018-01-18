2b76ec72cb4f9da2fdb622bde9ec184c85a2a9c6
fahri <fahri.petrucci@gmail.com>
2018-01-19 02:59:26 +0700

    Forth commit

---
+++ fellyModule/botLogin.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+from linepy import *
+import json, time, random, tempfile, os, sys
+from gtts import gTTS
+from googletrans import Translator
+from random import randint
+
+if (sys.version_info < (3, 0)):
+    print("The script requires Python 3x :( Sorry!\nrecommended using python 3.6")
+    sys.exit()
+
+from os import system, mkdir, getcwd, name
+from os.path import exists, join
+from textwrap import wrap
+
+from PyLyrics import *
+
+from importlib import reload
+
+reload(sys)
+print(sys.getdefaultencoding())
+
+#===========================================================================================================================================#
+
+client = LINE()
+# client = LINE(idOrAuthToken="YOUR_AUTH_TOKEN")
+client.log("Auth Token : " + str(client.authToken))
+
+channel = Channel(client,client.server.CHANNEL_ID['LINE_TIMELINE'])
+client.log("Channel Access Token : " + str(channel.getChannelResult()))
+
+poll = OEPoll(client)
+
+#===========================================================================================================================================#
+
+
+ki = LINE()
+ki.log("Auth Token : " + str(ki.authToken))
+
+kichannel = Channel(ki,ki.server.CHANNEL_ID['LINE_TIMELINE'])
+ki.log("Channel Access Token : " + str(kichannel.getChannelResult()))
+
+kipoll = OEPoll(ki)
+
+#===========================================================================================================================================#
+
+kc = LINE()
+kc.log("Auth Token : " + str(kc.authToken))
+
+kcchannel = Channel(kc,kc.server.CHANNEL_ID['LINE_TIMELINE'])
+kc.log("Channel Access Token : " + str(kcchannel.getChannelResult()))
+
+kcpoll = OEPoll(kc)
+
+#===========================================================================================================================================#
+
+kk = LINE()
+kk.log("Auth Token : " + str(kk.authToken))
+
+kkchannel = Channel(kk,kk.server.CHANNEL_ID['LINE_TIMELINE'])
+kk.log("Channel Access Token : " + str(kkchannel.getChannelResult()))
+
+kkpoll = OEPoll(kk)
+
+#===========================================================================================================================================#
+
+ks = LINE()
+ks.log("Auth Token : " + str(ks.authToken))
+
+kschannel = Channel(ks,ks.server.CHANNEL_ID['LINE_TIMELINE'])
+ks.log("Channel Access Token : " + str(kschannel.getChannelResult()))
+
+kspoll = OEPoll(ks)
+
+#===========================================================================================================================================#
+
+cl = client
+
+KAC=[cl,ki,kc,kk,ks]
+
+mid = cl.getProfile().mid #fahri
+Amid = ki.getProfile().mid #felly
+Bmid = kc.getProfile().mid #anuan
+Cmid = kk.getProfile().mid #andi
+Dmid = ks.getProfile().mid #agus
+
+
+Bots=[mid,Amid,Bmid,Cmid,Dmid]
